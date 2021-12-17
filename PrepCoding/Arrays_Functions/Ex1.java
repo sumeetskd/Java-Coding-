bf83c3bee1d3d4cad56fb4970a1735447894b8bc
summetskd <sumeetkumards@gmail.com>
2021-12-17 16:58:09 +0530

    final-counting-freq

--- PrepCoding/Arrays_Functions/Ex1.java
+++ PrepCoding/Arrays_Functions/Ex1.java
@@ -2,6 +2,17 @@ package PrepCoding.Arrays_Functions;
 import java.util.*;
 
 public class Ex1 {
+    public static int calcDigit(int n, int n2){
+        int count = 0;
+        int t = n;
+        while(t>0){
+            if(t%10==n2){
+                count++;
+            }
+            t/=10;
+        }
+        return count;
+    }
     public static void main(String args[]){
         //counting the number of repeating digits in a number
         Scanner sc = new Scanner(System.in);
@@ -10,14 +21,7 @@ public class Ex1 {
         System.out.println("Enter the digit to know it's frequency:");
         int n2 = sc.nextInt();
 
-        int count = 0;
-        int t = n1;
-        while(t>0){
-            if(t%10==n2){
-                count++;
-            }
-            t/=10;
-        }
-        System.out.println("Frequency of "+n2+" is "+count);
+        int res = calcDigit(n1,n2);
+        System.out.println("Frequency of "+n2+" is "+res);
     }
 }
