5a42c674b24d4f7b0ab6f58886a76d3f05a03745
summetskd <sumeetkumards@gmail.com>
2021-12-19 00:39:48 +0530

    new-change-Folderex5

---
+++ PrepCoding/Arrays_Functions/Ex5/Ex5.java
@@ -0,0 +1,22 @@
+package PrepCoding.Arrays_Functions.Ex5;
+import java.util.*;
+
+public class Ex5 {
+    public static int addition(int n1, int n2, int b){
+        int res = 0;
+
+        
+        return res;
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number:");
+        int n1 = sc.nextInt();
+        System.out.println("Enter the Second Number:");
+        int n2 = sc.nextInt();
+        System.out.println("Enter the base:");
+        int b = sc.nextInt();
+
+    }    
+}
