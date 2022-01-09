8daaeb3377c131de596cded9f6a87cbc0a79114f
summetskd <sumeetkumards@gmail.com>
2022-01-10 00:56:46 +0530

    correction

--- PrepCoding/Recursion/Factorial.java
+++ PrepCoding/Recursion/Factorial.java
@@ -1,18 +1,18 @@
 package PrepCoding.Recursion;
 import java.util.*;
 public class Factorial {
-    public static int Factorial(int n){
+    public static int factorial(int n){
         if(n==1||n==0){
             return n;
         }
-        int f = n*Factorial(n-1);
+        int f = n*factorial(n-1);
         return f;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number:");
         int n = sc.nextInt();
-        System.out.println("Factorial:"+Factorial(n));
+        System.out.println("Factorial:"+factorial(n));
         sc.close();
     }
 }
