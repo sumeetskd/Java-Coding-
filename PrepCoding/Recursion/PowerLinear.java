4025ab3cfd63911999c3abb68c8b148e798602b6
summetskd <sumeetkumards@gmail.com>
2022-01-10 01:16:40 +0530

    powerLinear

--- PrepCoding/Recursion/PowerLinear.java
+++ PrepCoding/Recursion/PowerLinear.java
@@ -1,5 +1,23 @@
 package PrepCoding.Recursion;
 
+import java.util.*;
+
 public class PowerLinear {
-    
+    public static int power(int n, int p){
+        if(p==0){
+            return 1;
+        }
+        int m = n*power(n,p-1);
+        return m;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        System.out.println("Enter the power:");
+        int p = sc.nextInt();
+        int k = power(n,p);
+        System.out.println("Power:"+k);
+        sc.close();
+    }
 }
