f2a8643ee81ec65adab8b3878d5c96e780781cd0
summetskd <sumeetkumards@gmail.com>
2022-01-14 00:48:26 +0530

    Array-print

---
+++ PrepCoding/Recursion/Array.java
@@ -0,0 +1,30 @@
+package PrepCoding.Recursion;
+
+import java.util.*;
+
+
+public class Array {
+    public static void displayArr(int[] arr, int idx){
+        if(idx<0){
+            return;
+        }
+        displayArr(arr,idx-1);
+        System.out.println(arr[idx]);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size:");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the array content:");
+        int arr[] = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements:");
+        displayArr(arr, n-1);
+        sc.close();
+    }
+}
